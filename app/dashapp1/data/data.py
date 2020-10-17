9d235cadc388c2e369571898ada53c3dfc44b79f
René Nadorp <rene.nadorp@inergy.nl>
2020-10-17 14:34:13 +0200

    Added PowerBI Dashboard to menu

--- app/dashapp1/data/data.py
+++ app/dashapp1/data/data.py
@@ -8,12 +8,12 @@ pd.options.display.max_rows = 99999999
 
 class Data(object):
     def get_data_confirmed(self):
-       
+
         df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
         dates = [c for c in df.columns if '20' in c]
         df_pivot=pd.melt(df, id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], value_vars=dates)
         df_pivot.rename(columns={"variable": "Date", "value": "CountConfirmed", "Country/Region":"Country", "Province/State":"State"}, inplace=True)
-        
+
         df_pivot['State'] = np.where(df_pivot['State'].isna(), df_pivot['Country'], df_pivot['State'])
         df_pivot[['Month','Day', 'Year']] =df_pivot.Date.str.split(pat = "/", expand=True)
         df_pivot['Year'] = '20' + df_pivot['Year'].astype(str)
@@ -21,10 +21,13 @@ class Data(object):
         df_pivot['Date'] = pd.to_datetime(df_pivot[['Year', 'Month' , 'Day']])
         df_pivot['Count'] = df_pivot["CountConfirmed"]
         df_pivot['CountConfirmedPrevious'] = (df_pivot.sort_values(by=['Country', 'State', 'Date'], ascending=True)
-                       .groupby(['Country', 'State'])['CountConfirmed'].shift(1))
+                    .groupby(['Country', 'State'])['CountConfirmed'].shift(1))
         df_pivot['CountConfirmedIncrease'] = df_pivot['CountConfirmed']-df_pivot['CountConfirmedPrevious']
-
+        df_pivot['HasConfirmed']= np.where((df_pivot['CountConfirmed']>0), 1, 0)
+        df_pivot['Seq'] = (df_pivot.sort_values(by=['Country', 'State', 'Date'], ascending=True)
+                    .groupby(['Country', 'State'])['HasConfirmed'].cumsum())
         return df_pivot.reset_index().fillna(0)[df_pivot['Count']>=0]
+
 
     def get_data_deaths(self):
        
